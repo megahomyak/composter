0edf475c3e66cc4d2b692e6a351f62afe108db21
megahomyak <g.megahomyak@gmail.com>
2024-09-17 21:06:12 +0500

    An interesting idea. Unlikely to finish

---
+++ composter/__init__.py
@@ -0,0 +1,18 @@
+class Element:
+
+    def get_styles(self):
+        pass
+
+    def get_children(self):
+        pass
+
+    def get_
+
+page = compose([
+    Header(
+        text="Title",
+    ),
+    HorizontallyRestricted([
+
+    ]),
+])
